b6da44a5fd66a0f3aa48d42717419b461336e251
Artem <artem.kyiv@outlook.com>
2019-06-22 17:29:47 +0300

    fixed string revision value

--- Formula/now.rb
+++ Formula/now.rb
@@ -4,14 +4,10 @@ class Now < Formula
   url "https://mainstreamer.github.io/homebrew-now/now"
   sha256 "a70c5ae06c7079de89458d18a3ebfde3a5edd3f0c8e671a432027270e3de9b91"
   version "0.1"
-  revision "1"
+  revision 1
   bottle :unneeded
-
   def install
   	bin.install "now"
     puts 'INSTALL SUCCESSFULL'
   end
- test do
- 	puts 'TEST OK'
- end
 end
