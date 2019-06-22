78a4a8e445d52edafc60748686f58283ed394f15
Artem <artem.kyiv@outlook.com>
2019-06-22 17:35:56 +0300

    added check for .now existance

--- Formula/now.rb
+++ Formula/now.rb
@@ -2,7 +2,7 @@ class Now < Formula
   desc "Open Source Docker Helper Utility"
   homepage "https://mainstreamer.github.io/homebrew-now/"
   url "https://mainstreamer.github.io/homebrew-now/now"
-  sha256 "09d71767e527abdf6fb21cc6ea36728d6a66a5f6fa9a6c51f6a280d6da1ef4a6"
+  sha256 "f843757d8bb24e9188e5de13f84594e36f6a6007991bc60775c4c257c982c3fe"
   version "0.1"
   revision 1
   bottle :unneeded
